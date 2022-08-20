6aa8ebcf6bd61d3d052a54c81c1070e6731411ec
VladV1V <mahno444@gmail.com>
2022-08-19 21:07:54 -0600

    Create Library, Settings, Updates screens

---
+++ app/src/main/java/com/vladv1v/mangazanime/ui/moresettings/MoreSettingsScreen.kt
@@ -0,0 +1,13 @@
+package com.vladv1v.mangazanime.ui.moresettings
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MoreSettingsScreen() {
+    Surface(modifier = Modifier.fillMaxSize()) {
+
+    }
+}
