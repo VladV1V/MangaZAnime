3a4818e0eba9cedff256710d13db294972601232
VladV1V <mahno444@gmail.com>
2022-08-22 14:06:26 -0600

    Create basic navigation between screen

--- app/src/main/java/com/vladv1v/mangazanime/MainActivity.kt
+++ app/src/main/java/com/vladv1v/mangazanime/MainActivity.kt
@@ -3,25 +3,91 @@ package com.vladv1v.mangazanime
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.vladv1v.mangazanime.ui.library.LibraryListScreen
+import com.vladv1v.mangazanime.ui.moresettings.SettingsScreen
 import com.vladv1v.mangazanime.ui.theme.MangaZAnimeTheme
+import com.vladv1v.mangazanime.ui.updates.UpdatesScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MangaZAnimeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-
-                }
+                val navController = rememberNavController()
+                Navigation(navController)
             }
         }
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Navigation(navController: NavHostController) {
+    Scaffold(
+        bottomBar = {
+            BottomAppBar(modifier = Modifier.wrapContentHeight()) {
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val currentDestination = navBackStackEntry?.destination
+                items.forEach { screen ->
+                    NavigationBarItem(
+                        icon = { Icon(painter = painterResource(id = screen.icon) , contentDescription = screen.title, modifier = Modifier.size(24.dp))},
+                        label = { Text(text = screen.title) },
+                        selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                        onClick = {
+                            navController.navigate(screen.route) {
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+
+                        }
+                    )
+                }
+            }
+        }
+    ) { paddingValues ->
+        NavHost(
+            navController = navController,
+            startDestination = ScreenL.LibraryList.route,
+            modifier = Modifier.padding(paddingValues)
+        ) {
+            composable(ScreenL.LibraryList.route) { LibraryListScreen() }
+            composable(ScreenL.UpdatesList.route) { UpdatesScreen() }
+            composable(ScreenL.Settings.route) { SettingsScreen() }
+        }
+    }
+}
+
+sealed class ScreenL(var title: String, @DrawableRes val icon: Int, val route: String) {
+    object LibraryList : ScreenL("Library", R.drawable.book, "library_list")
+    object UpdatesList : ScreenL("Updates", R.drawable.updates, "updates")
+    object Settings : ScreenL("Settings", R.drawable.settings, "settings")
+}
+
+val items = listOf(
+    ScreenL.LibraryList,
+    ScreenL.UpdatesList,
+    ScreenL.Settings
+)
--- app/src/main/java/com/vladv1v/mangazanime/ui/library/LibraryListScreen.kt
+++ app/src/main/java/com/vladv1v/mangazanime/ui/library/LibraryListScreen.kt
@@ -1,9 +1,14 @@
 package com.vladv1v.mangazanime.ui.library
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun LibraryListScreen() {
